705069bb99ff71cdb002f6583c28b792c1c1b2dc
Avnish Ranjan <avnishranjan7@gmail.com>
2026-03-09 13:02:14 +0530

    SELF HOSTED RUNNER DEPLOY

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
-from flask import Flask, jsonify, render_template
-
-app = Flask(__name__, template_folder="templetes")
-
-
-@app.route("/")
-def home():
-    return render_template("index.html")
+from flask import Flask, render_template
+app = Flask(__name__)
 
 
-@app.route("/health")
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/health')
 def health():
-    return jsonify(status="ok")
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    return 'Server is up and running'
