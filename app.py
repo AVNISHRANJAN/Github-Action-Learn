729cbc4463b5d7cec067c9f677be2e3d3afe8ed9
Avnish Ranjan <avnishranjan7@gmail.com>
2026-03-11 17:17:26 +0530

    app.py change

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+
+app = Flask(__name__, template_folder="templetes")
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/health')
+@app.route("/health")
 def health():
-    return 'Server is up and running'
+    return "Server is up and running"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
